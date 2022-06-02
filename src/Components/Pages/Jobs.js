05d3fb4136e9d23e62b231f50f901b038866a542
ullash4 <programmerullash@gmail.com>
2022-06-02 13:51:40 +0600

    fetch data

--- src/Components/Pages/Jobs.js
+++ src/Components/Pages/Jobs.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react'
+import Loading from '../Shared/Loading'
 
 function Jobs() {
   const [jobs, setJobs] = useState([])
+  const [isLoading, setLoading]= useState(true)
   useEffect(()=>{
-    fetch('http://refertest.pythonanywhere.com/job/openings',{
-      method:'GET',
-      headers:{
-        "tid":3441
-      }
-    })
+    fetch('http://localhost:5000/jobs')
     .then(res=>res.json())
     .then(data=>{
-      setJobs(data.data)
+      setJobs(data)
       console.log(data);
+      setLoading(false)
     })
   },[])
+  if(isLoading){
+    return <Loading />
+  }
   return (
     <div>Jobs{jobs.length}</div>
   )
